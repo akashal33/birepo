be926061a889adeff64136ffa2d0ce723f949ae7
akash <akashk33@hotmail.com>
2023-02-15 09:59:35 +0530

    second commit

--- src/main/java/com/akashk/rest/controller/BenefitIssuanceRest.java
+++ src/main/java/com/akashk/rest/controller/BenefitIssuanceRest.java
@@ -2,6 +2,7 @@ package com.akashk.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class BenefitIssuanceRest {
 	@Autowired
 	private BenefitIssuanceService benefitIssuanceService;
 
-	@PostMapping("/generate")
+	@GetMapping("/generate")
 	public ResponseEntity<String> generate(){
 		
 		benefitIssuanceService.generate();
